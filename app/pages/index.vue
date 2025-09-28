9bc1bd2acf898a03032c61a36aad9195c72dfdb9
gharakhan <egharakhan90@gmail.com>
2025-09-28 14:44:54 +0330

    fix bug

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,15 +2,15 @@
   <div>
     <h1>Welcome to the homepage</h1>
   </div>
-  <hr>
-  <div class="mx-auto m-6 flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-    <img class="size-12 shrink-0" src="" alt="ChitChat Logo" />
-    <div>
-      <div class="text-xl font-medium text-black dark:text-white">ChitChat</div>
-      <p class="text-gray-500 dark:text-gray-400">You have a new message!</p>
-    </div>
-  </div>
-  <hr>
+<!--  <hr>-->
+<!--  <div class="mx-auto m-6 flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">-->
+<!--    <img class="size-12 shrink-0" src="" alt="ChitChat Logo" />-->
+<!--    <div>-->
+<!--      <div class="text-xl font-medium text-black dark:text-white">ChitChat</div>-->
+<!--      <p class="text-gray-500 dark:text-gray-400">You have a new message!</p>-->
+<!--    </div>-->
+<!--  </div>-->
+<!--  <hr>-->
   <div class="p-4">
     <div v-if="loading" class="mt-4">Loading...</div>
     <div v-else-if="error" class="mt-4 text-red-500">{{ error }}</div>
